833a078716b2c72ef6cabab78c0d4cceb2d18ee9
Zama Bandeira Braga <zamabandeirabr@gmail.com>
2022-04-07 15:03:12 -0300

    chore: update example

--- examples/main.tf
+++ examples/main.tf
@@ -1,10 +1,5 @@
 module "example" {
-  source  = "./../"
-  solucao = "example"
+  source   = "./../"
+  solucao  = "example"
   ambiente = "hml"
-  unidade= "corporate"
-  diretoria = "teste"
-  superintendencia = "teste"
-  gerencia = "teste"
-  squad = "teste" 
 }
